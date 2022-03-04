ca0e3cdce4c018f06991d3d3e7b91e9c01eb2fd1
usama203 <m.usamariaz88@gmail.com>
2022-03-04 23:57:03 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
+
+import { Datepicker, Page, setOptions } from '@mobiscroll/react';
+
+setOptions({
+    theme: 'ios',
+    themeVariant: 'dark'
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Page>
+            <Datepicker
+                controls={['datetime']}
+                stepMinute={15}
+                inputProps={{
+                    label: 'Date & Time (every 15 min)',
+                    labelStyle: 'stacked',
+                    inputStyle: 'outline',
+                    placeholder: 'Please Select...'
+                }}
+            />
+            <Datepicker
+                controls={['time']}
+                stepMinute={5}
+                inputProps={{
+                    label: 'Time (every 5 min)',
+                    labelStyle: 'stacked',
+                    inputStyle: 'outline',
+                    placeholder: 'Please Select...'
+                }}
+            />
+            <Datepicker
+                controls={['time']}
+                stepHour={2}
+                inputProps={{
+                    label: 'Time (every 2 hours)',
+                    labelStyle: 'stacked',
+                    inputStyle: 'outline',
+                    placeholder: 'Please Select...'
+                }}
+            />
+            <Datepicker
+                controls={['time']}
+                stepSecond={30}
+                timeFormat="HH:mm:ss"
+                inputProps={{
+                    label: 'Time (every 30 seconds)',
+                    labelStyle: 'stacked',
+                    inputStyle: 'outline',
+                    placeholder: 'Please Select...'
+                }}
+            />
+        </Page>
+    
+    
+    </>
   );
 }
 
